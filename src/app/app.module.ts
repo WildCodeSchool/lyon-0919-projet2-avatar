c63d38d66c21a0da62016b7f99be1df28a20d98a
Lisa THAZAR <lisa.thazar@hotmail.fr>
2019-10-22 17:56:25 +0200

    Assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AvatarComponent } from './avatar/avatar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatarComponent
   ],
   imports: [
     BrowserModule,
