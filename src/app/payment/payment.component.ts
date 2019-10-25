02e6921d4b1bc657c9d977bac56bb2fc9ee11782
Thomas <tponthoreau@gmail.com>
2019-10-25 09:20:25 +0200

    Modal if payment finished
    If the form payment valid, modal display,
    Boutton qui retourne sur la page home une fois cliqué.

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Payment } from './payment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -10,13 +11,24 @@ export class PaymentComponent implements OnInit {
 
   model: Payment = new Payment();
 
-  
-  constructor() { }
+  isFormVisible: boolean = true;
+  isModalVisible: boolean = false;
+  constructor(private router: Router) { }
   
   ngOnInit() {
   }
   
   onSubmit() {
     console.log(this.model);
+    this.isFormVisible = false;
+    this.isModalVisible = true;
+  };
+
+  onModal() { 
+    
+  };
+
+  closeModal() {
+    this.router.navigate(['/home']);
   }
 }
