e5a8f58792a42c76839c09d6f837d92a37922d74
Thomas <tponthoreau@gmail.com>
2019-11-06 15:59:32 +0100

    card generator

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cards } from '../shared/cards';
+import { Card } from '../shared/card';
 
 @Component({
   selector: 'app-cards',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+  
+  displayCards: Cards = new Cards;
+  card: Card;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  onCardSelected(c: Card) {
+    this.displayCards.card = c;
+  }
 }
